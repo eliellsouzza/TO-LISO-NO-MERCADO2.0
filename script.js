aeb1daf9a01c3ae08b458cc4f7ad5fcf5f7f2092
Eliel Souza <eliellsouzza@gmail.com>
2023-11-26 10:12:31 -0300

    v3.0

--- script.js
+++ script.js
@@ -1,98 +1,95 @@
-const btn = document.querySelector("#send");
 
-// Adiciona um evento de clique ao botão
-btn.addEventListener("click", function (e) {
-    e.preventDefault();
+function adicionarProduto() {
+    const produto = document.getElementById("produto").value;
+    const quantidade = parseInt(document.getElementById("quantidade").value, 10);
+    const preco = parseFloat(document.getElementById("preco").value);
 
-    // Captura os valores dos inputs
-    const name = document.querySelector("#name").value;
-    const qtd = document.querySelector("#qtd").value;
-    const preco = document.querySelector("#preco").value;
+    if (produto.trim() !== "" && !isNaN(quantidade) && !isNaN(preco)) {
+        const tabelaProdutos = document.querySelector("#tabela-produtos");
+        const newRow = tabelaProdutos.insertRow();
+        newRow.className = "produto";
+        
+        const cellNome = newRow.insertCell(0);
+        const cellQuantidade = newRow.insertCell(1);
+        const cellPreco = newRow.insertCell(2);
+        const cellAcoes = newRow.insertCell(3);
 
-    // Limpa os inputs
-    document.querySelector("#name").value = '';
-    document.querySelector("#qtd").value = '';
-    document.querySelector("#preco").value = '';
+        cellNome.textContent = produto;
+        cellQuantidade.innerHTML = `<input type="number" value="${quantidade}" onchange="atualizarTotalGeral()">`;
+        cellPreco.textContent = preco.toFixed(2);
+        cellAcoes.innerHTML = `
+            <button class="editar-edit" onclick="editarProduto(this)">Editar</button>
+            <button class="excluir-edit" onclick="excluirProduto(this)">Excluir</button>
+        `;
+        
+        atualizarTotalGeral();
+    }
 
-    // Adiciona os valores à lista de produtos (usando uma <ul>)
-    const listaProdutos = document.querySelector("#lista-produtos");
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `Nome: ${name}, Quantidade: ${qtd}, Preço: ${preco} <button class="btn-excluir" onclick="excluirItemLista(this)">Excluir</button>`;
-    listaProdutos.appendChild(listItem);
-
-    // Adiciona os valores à tabela de produtos (usando uma <tbody>)
-    const tabelaProdutos = document.querySelector("#tabela-produtos");
-    const newRow = tabelaProdutos.insertRow();
-    const cellNome = newRow.insertCell(0);
-    const cellQuantidade = newRow.insertCell(1);
-    const cellPreco = newRow.insertCell(2);
-    const cellAcoes = newRow.insertCell(3);
-
-   /*  cellNome.textContent = name;
-    cellQuantidade.textContent = qtd;
-    cellPreco.textContent = preco;
-    cellAcoes.innerHTML = `<button class="btn-excluir" onclick="excluirItemTabela(this)">Excluir</button>`;
- */
-    // Atualiza o total geral
-    atualizarTotalGeral();
-});
-
-function atualizarTotalGeral() {
-    let totalGeral = 0;
-
-    // Percorre os itens da lista ou da tabela e calcula o total
-    const listaItens = document.querySelectorAll("#lista-produtos li");
-    listaItens.forEach(item => {
-        // Correção na expressão regular para extrair o valor do preço
-        const precoMatch = item.textContent.match(/Preço: ([\d.]+)/);
-        if (precoMatch) {
-            const preco = parseFloat(precoMatch[1]);
-            totalGeral += preco;
-        }
-    });
-
-    // Atualiza o total geral na página
-    document.getElementById("total-valor").textContent = totalGeral.toFixed(2);
+    // Limpar campos após adicionar produto
+    document.getElementById("produto").value = "";
+    document.getElementById("quantidade").value = "";
+    document.getElementById("preco").value = "";
 }
 
-function excluirItemLista(button) {
-    const listItem = button.parentNode;
-    listItem.parentNode.removeChild(listItem);
+function editarProduto(button) {
+    const row = button.parentNode.parentNode;
+    const nome = row.cells[0].textContent;
+    const quantidade = parseInt(row.cells[1].querySelector("input").value, 10);
+    const preco = parseFloat(row.cells[2].textContent);
+
+    document.getElementById("produto").value = nome;
+    document.getElementById("quantidade").value = quantidade;
+    document.getElementById("preco").value = preco.toFixed(2);
+
+    // Remover a linha da tabela após editar
+    row.parentNode.removeChild(row);
+
     atualizarTotalGeral();
 }
 
-function excluirItemTabela(button) {
+function excluirProduto(button) {
     const row = button.parentNode.parentNode;
     row.parentNode.removeChild(row);
     atualizarTotalGeral();
 }
 
+function atualizarTotalGeral() {
+    let totalGeral = 0;
+    const produtos = document.querySelectorAll(".produto");
+
+    produtos.forEach(produto => {
+        const quantidade = parseInt(produto.cells[1].querySelector("input").value, 10);
+        const preco = parseFloat(produto.cells[2].textContent);
+        totalGeral += quantidade * preco;
+    });
+
+    document.getElementById("total-valor").textContent = totalGeral.toFixed(2);
+}
+
+
 
 
 function exibirPopup() {
-    const overlay = document.getElementById("overlay");
-    overlay.style.display = "block";
+const overlay = document.getElementById("overlay");
+overlay.style.display = "block";
 }
 
 function fecharPopup() {
-    const overlay = document.getElementById("overlay");
-    overlay.style.display = "none";
+const overlay = document.getElementById("overlay");
+overlay.style.display = "none";
 }
 
 function exibirBoasVindas() {
-    const nomeInput = document.getElementById("input-nome").value;
-    const nomeUsuario = document.getElementById("nome-usuario");
+const nomeInput = document.getElementById("input-nome").value;
+const nomeUsuario = document.getElementById("nome-usuario");
 
-    if (nomeInput.trim() !== "") {
-        nomeUsuario.textContent = nomeInput;
-        fecharPopup();
-    }
+if (nomeInput.trim() !== "") {
+    nomeUsuario.textContent = nomeInput;
+    fecharPopup();
+}
 }
 
 // Exibe o popup ao carregar a página
 window.onload = function () {
-    exibirPopup();
+exibirPopup();
 };
-
-
-
